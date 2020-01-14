11a1182193fda87541959bcfcc617b313043034b
Milo Rastgoo <hello@informagician.com>
2020-01-14 17:28:38 -0500

    User component added

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import User from './components/User';
 
 class App extends React.Component {
 
@@ -19,7 +20,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>{this.state.userData.login}</h1>
+        <User user={this.state.userData} />
       </div>
     );
   }
---
+++ github-user-card/src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const User = props => {
+
+    return(
+        <h1>{props.user.login}</h1>
+    )
+}
+
+export default User;
