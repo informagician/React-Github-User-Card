6253fb8d94dea285d767defd07106b6ba8fc8c87
Milo Rastgoo <hello@informagician.com>
2020-01-14 19:18:49 -0500

    search function added

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -8,28 +8,63 @@ import Followers from './components/Followers';
 class App extends React.Component {
 
   state = {
+    userName: 'informagician',
     userData:[],
+    userContributions: [],
     followersData: []
   };
 
   componentDidMount() {
     axios
-      .get('https://api.github.com/users/informagician')
+      .get(`https://api.github.com/users/${this.state.userName}`)
       .then(res => this.setState({ userData: res.data }))
       .catch(err => console.log(err));
 
       axios
-      .get(`https://api.github.com/users/informagician/followers`)
+      .get(`https://api.github.com/users/${this.state.userName}/followers`)
       .then(res => this.setState({ followersData: res.data }))
       .catch(err => console.log(err));
   }
 
+  componentDidUpdate(prevProps,prevState){
+    if(this.state.userName !== prevState.userName) {
+      if(this.state.userName == 'informagician') {
+        
+      }
+    }
+  }
+
+
+  handleChanges = e => {
+    this.setState({
+      userName: e.target.value
+    });
+  };
+
+  fetchUser = e => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}`)
+      .then(res => this.setState({ userData: res.data }))
+      .catch(err => console.log(err));
+
+      axios
+      .get(`https://api.github.com/users/${this.state.userName}/followers`)
+      .then(res => this.setState({ followersData: res.data }))
+      .catch(err => console.log(err));
+  };
 
   // login, avatar_url , name , location , bio , followers , following
   render() {
     return (
       <div className="app">
         <h1>GitHub User Card Assignment</h1>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={this.handleChanges}
+        />
+        <button onClick={this.fetchUser}>Search</button>
         <User user={this.state.userData} />
         <h2>Followers</h2>
         <Followers follower={this.state.followersData} />
